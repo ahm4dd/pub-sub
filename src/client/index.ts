fccfac57d03381b724e28d560fa174c1cbfbde1b
ahm4dd <ahmed.emad.edu@gmail.com>
2025-11-27 18:36:58 +0300

    feat: added a way to create and bind a queue (client side)

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,29 @@
+import amqp from "amqplib";
+import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import { declareAndBind } from "../internal/pubsub/index.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+
 async function main() {
   console.log("Starting Peril client...");
+
+  const connString = "amqp://guest:guest@localhost:5672";
+  const conn = await amqp.connect(connString);
+
+  console.log("Connected successfully");
+
+  process.on("exit", async () => {
+    console.log("\nProgram shutting down...");
+    await conn.close();
+  });
+
+  const name = await clientWelcome();
+  const channelAndQueue = await declareAndBind(
+    conn,
+    ExchangePerilDirect,
+    `${PauseKey}.${name}`,
+    PauseKey,
+    "transient"
+  );
 }
 
 main().catch((err) => {
--- src/internal/pubsub/index.ts
+++ src/internal/pubsub/index.ts
@@ -1,4 +1,7 @@
-import type { ConfirmChannel } from "amqplib";
+import type { ConfirmChannel, ChannelModel } from "amqplib";
+import amqp from "amqplib";
+
+export type SimpleQueueType = "durable" | "transient";
 
 export async function publishJSON<T>(
   ch: ConfirmChannel,
@@ -10,4 +13,24 @@ export async function publishJSON<T>(
   ch.publish(exchange, routingKey, Buffer.from(valueJSON), {
     contentType: "application/json",
   });
+}
+
+export async function declareAndBind(
+  conn: amqp.ChannelModel,
+  exchange: string,
+  queueName: string,
+  key: string,
+  queueType: SimpleQueueType
+): Promise<[amqp.Channel, amqp.Replies.AssertQueue]> {
+  const channel = await conn.createChannel();
+
+  const queue = await channel.assertQueue(queueName, {
+    autoDelete: queueType === "transient" ? true : false,
+    exclusive: queueType === "transient" ? true : false,
+    durable: queueType === "durable" ? true : false,
+    arguments: null,
+  });
+
+  await channel.bindQueue(queueName, exchange, key);
+  return [channel, queue];
 }
