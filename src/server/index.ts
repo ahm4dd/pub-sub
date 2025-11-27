00a86aa30553e04e3a08b858f24e27ab37441292
ahm4dd <ahmed.emad.edu@gmail.com>
2025-11-27 15:15:57 +0300

    Basic amqp connection, docker file for STOMP rabbitmq plugin

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,13 @@
+import amqp from "amqplib";
 async function main() {
   console.log("Starting Peril server...");
+  const connString = `amqp://guest:guest@localhost:5672/`;
+  let conn = await amqp.connect(connString);
+  console.log("Connected successfully");
+  process.on("exit", async () => {
+    console.log("\nProgram shutting down...");
+    await conn.close();
+  });
 }
 
 main().catch((err) => {
